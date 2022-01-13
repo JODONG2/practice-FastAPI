9bd7487d7a6f0836110a496cf4f61479b4ed36eb
JODONG2 <whgusehd96@gmail.com>
2022-01-13 23:17:50 +0900

    feat: Optional,query parameter

--- FastAPI_2/getMethod.py
+++ FastAPI_2/getMethod.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 import uvicorn 
-
+from typing import Optional 
 
 app = FastAPI()
+
+career = [{"CV": "my career"}, {"NLP":"your career"}, {"MLOps":"our career" }]
+
+@app.get("/career")
+def get_career(first:int = 0, limit:int = 1):
+    return career[first:first+limit]
+
+@app.get("/users/{user_id}")
+def get_user2(user_id:str, option:Optional[str]= None):
+    if option:
+        return {user_id:option}
+    return {user_id:user_id+" and option is None"}
 
 @app.get("/users/{user_id}")
 def get_user(user_id):
